34ebde4fdd92b89debccb639c02dd92ce6d19d9b
bsjin <bsjin7231@gmail.com>
2020-11-13 18:22:40 +0900

    Rmi Client

---
+++ src/kr/or/ddit/inf/ClientChatInf.java
@@ -0,0 +1,12 @@
+package kr.or.ddit.inf;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ClientChatInf extends Remote{
+
+		//서버가 보내온 메시지를 화면에 출력하는 메소드
+
+		public void printMessage(String msg) throws RemoteException;
+
+}
